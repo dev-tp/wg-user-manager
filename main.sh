1c84849b40bdc1f0fe19d00091a2474aaf96e7aa
Erik Sorto <rcksorto@yahoo.com>
2023-10-14 21:42:07 -0700

    Add ability to create VPN users

--- main.sh
+++ main.sh
@@ -1,8 +1,43 @@
 #!/bin/sh
 
-# TODO Create profile
 function create {
-  echo 'create'
+  if [ ! -f wg.db ]; then
+    sqlite3 -line wg.db 'CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, profile TEXT, address TEXT, public_key TEXT, private_key TEXT);'
+  fi
+
+  wg genkey | tee private.key | wg pubkey > public.key
+
+  private_key=$(cat private.key)
+  public_key=$(cat public.key)
+
+  printf "Who is going to use this connection? "
+  read profile
+
+  printf "What address will be assigned (e.g. 10.0.0.x)? "
+  read address
+
+cat << EOF > CTL.conf
+[Interface]
+Address = $address/32
+PrivateKey = $private_key
+
+[Peer]
+AllowedIPs = 0.0.0.0/1, 128.0.0.0/1
+Endpoint = office.countrywidetriallawyers.com:51820
+PublicKey = GzQYjKqA0bsyCC6e3N8zz2kShGMsLP9SHZgWz4ezgwM=
+EOF
+
+  zip config.zip CTL.conf
+  rm CTL.conf
+
+  # Perhaps give the option to create a directory whenever multiple users are
+  # added sequentially
+  # mkdir -p "$profile"
+  # mv config.zip "$profile"
+
+  sqlite3 -line wg.db "INSERT INTO user (profile, address, public_key, private_key) VALUES ('$profile', '$address', '$public_key', '$private_key');"
+
+  rm *.key
 }
 
 # TODO Delete profile
@@ -10,12 +45,14 @@ function delete {
   echo 'delete'
 }
 
-# TODO List profiles
 function list {
-  echo 'list'
+  sqlite3 -line wg.db 'SELECT * FROM user;'
 }
 
 # TODO Print generated wg0.conf file
 function print {
   echo 'print'
 }
+
+create
+list
