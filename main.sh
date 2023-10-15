2de6a293dd07bc054350e445e133241ddca1999a
Erik Sorto <rcksorto@yahoo.com>
2023-10-14 19:30:00 -0700

    Initial commit

---
+++ main.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# TODO Create profile
+function create {
+  echo 'create'
+}
+
+# TODO Delete profile
+function delete {
+  echo 'delete'
+}
+
+# TODO List profiles
+function list {
+  echo 'list'
+}
+
+# TODO Print generated wg0.conf file
+function print {
+  echo 'print'
+}
