581d5ab11aab35b95ca69abfff280f625d3ccbeb
Erik Sorto <rcksorto@yahoo.com>
2023-10-15 22:42:46 -0700

    Main file requires `bash` to run
    - Arrays are not supported outside bash
    - File can be formatted with `shfmt`

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 function create {
   if [ ! -f wg.db ]; then
-sqlite3 wg.db << EOF
+    sqlite3 wg.db <<EOF
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   profile TEXT,
@@ -15,7 +15,7 @@ CREATE TABLE user (
 EOF
   fi
 
-  wg genkey | tee private.key | wg pubkey > public.key
+  wg genkey | tee private.key | wg pubkey >public.key
 
   private_key=$(cat private.key)
   public_key=$(cat public.key)
@@ -25,7 +25,7 @@ EOF
   read -p 'Who is going to use this connection? ' profile
   read -p 'What address will be assigned (e.g. 10.0.0.x)? ' address
 
-cat << EOF > CTL.conf
+  cat <<EOF >CTL.conf
 [Interface]
 Address = $address/32
 PrivateKey = $private_key
@@ -44,7 +44,7 @@ EOF
   # mkdir -p "$profile"
   # mv config.zip "$profile"
 
-sqlite3 wg.db << EOF
+  sqlite3 wg.db <<EOF
 INSERT INTO user (profile, address, private_key, public_key)
 VALUES ('$profile', '$address', '$private_key', '$public_key');
 EOF
@@ -68,31 +68,31 @@ function edit {
   if [[ $id =~ ^[0-9]+$ ]]; then
     sqlite3 wg.db "SELECT profile, address FROM user WHERE id = $id" | {
       while IFS='|' read -ra row; do
-        read -p "Enter profile [${row[0]}]: " profile < /dev/tty
+        read -p "Enter profile [${row[0]}]: " profile </dev/tty
 
         if [ -z "$profile" ]; then
           profile=${row[0]}
         fi
 
-        read -p "Enter address [${row[1]}]: " address < /dev/tty
+        read -p "Enter address [${row[1]}]: " address </dev/tty
 
         if [ -z "$address" ]; then
           address=${row[1]}
         fi
 
-        read -p 'Would you like to regenerate key pair? [y/N] ' regenerate < /dev/tty
+        read -p 'Would you like to regenerate key pair? [y/N] ' regenerate </dev/tty
 
         if [[ "$regenerate" != 'y' ]]; then
           sqlite3 wg.db "UPDATE user SET profile = '$profile', address = '$address' WHERE id = $id"
         else
-          wg genkey | tee private.key | wg pubkey > public.key
+          wg genkey | tee private.key | wg pubkey >public.key
 
           private_key=$(cat private.key)
           public_key=$(cat public.key)
 
           rm *.key
 
-sqlite3 wg.db << EOF
+          sqlite3 wg.db <<EOF
 UPDATE
   user
 SET
@@ -114,7 +114,7 @@ function list {
 }
 
 function print {
-cat << EOF
+  cat <<EOF
 [Interface]
 Address = 10.0.0.1/24
 ListenPort = 51820
