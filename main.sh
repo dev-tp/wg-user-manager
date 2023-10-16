69f15bfaea0053911dcbd68a35108e7d37edd01f
Erik Sorto <rcksorto@yahoo.com>
2023-10-15 21:18:42 -0700

    Use `read -p` instead of `printf; read` combo

--- main.sh
+++ main.sh
@@ -18,11 +18,8 @@ EOF
   private_key=$(cat private.key)
   public_key=$(cat public.key)
 
-  printf "Who is going to use this connection? "
-  read profile
-
-  printf "What address will be assigned (e.g. 10.0.0.x)? "
-  read address
+  read -p 'Who is going to use this connection? ' profile
+  read -p 'What address will be assigned (e.g. 10.0.0.x)? ' address
 
 cat << EOF > CTL.conf
 [Interface]
